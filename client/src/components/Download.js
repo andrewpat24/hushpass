709e2cdcc3c8c13e84864042d064356b353f559b
HomeBase <pmalolep@mail.sfsu.edu>
2019-05-14 16:14:47 -0700

    working file delivery with password

--- client/src/components/Download.js
+++ client/src/components/Download.js
@@ -4,86 +4,44 @@ import axios from 'axios';
 class Download extends Component{
     constructor(props) {
         super(props);
-        this.state = {document:{}}
+        this.state = {
+            document:{},
+            pw:''
+        }
     }
-
-    test = () => {
-        const data = new FormData();
-        
-        data.append('password',document.getElementById('password').value);
-        const config = {responseType: 'arraybuffer', headers:{'Content-Type':'multipart/form-data'}};
-
-        // window.location = `/api/db/download/${this.props.match.params.fileId}`;
-
-        fetch("/api/db/test/"+this.props.match.params.fileId, data, config)
-        
-        .then((res) => { 
-            console.log('res:',res);
-            // const filename = res.headers["content-disposition"].split('=')[1].replace('"','').replace('"','');
-            
-            console.log('filename:',this.state.document.fileName);
-
-
-            // const url = window.URL.createObjectURL(new Blob([res.blob()]));
-            // const link = document.createElement('a');
-            // link.href = url;
-            // alert(url);
-            // link.setAttribute('download',this.state.document.fileName);
-            // document.body.appendChild(link);
-            // link.click();
-
-        })
-        .catch(err => { 
-            console.log("Download Failed\n"+err);
-        })
-
-    }
-
 
     onClickHandler = () => {
 
         const data = new FormData();
-        
         data.append('password',document.getElementById('password').value);
         const config = {responseType: 'arraybuffer', headers:{'Content-Type':'multipart/form-data', 'accept':''}};
         
-        console.log('getting file');
         axios.post("/api/db/file/"+this.props.match.params.fileId, data, config)
-        // fetch("/api/db/file/"+this.props.match.params.fileId, {password:document.getElementById('password').value})
         .then( res => {
-            // filename = res.headers["Content-Disposition"].split('=')[1].replace('"','').replace('"','');;
-            console.log(res);
-            return res.blob();
-        })
-        .then( blob =>{
-            console.log(blob);
-            var url = window.URL.createObjectURL(blob);
-            var a = document.createElement('a');
-            a.href = url;
-            a.download = this.state.document.fileName;
-            a.type = this.state.document.fileType;
-            document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-            a.click();    
-            a.remove();
+            this.setState({pw: ''});
+            const url = window.URL.createObjectURL(new Blob([res.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = this.state.document.fileName;
+            link.type = this.state.document.fileType;
+            document.body.appendChild(link);
+            link.click();
         })
         .catch( err =>{
             console.error('Error:',err);
+            this.setState({pw: 'Bad Password'});
         })
 
     }
 
-    componentDidMount(){ window.addEventListener('load', async ()=>{
-        console.log('did mount');
+    componentDidMount(){ window.addEventListener('load',  async () => {
         const doc = await(await fetch("/api/db/"+this.props.match.params.fileId)).json();
         this.setState({document: doc});
-        // console.log(doc);
-        console.log( this.state.document );
-    })}
+
+    } )}
 
     render = () => {
-
-        return (
-            <div>
+        const successTemplate = (<div>
             <h1>Download Page</h1>
                 <div> 
                     File<br/>
@@ -93,11 +51,21 @@ class Download extends Component{
                     Expiration: {this.state.document.expirationDate}<br/>
 
                     <input type='text' id='password' name='password' placeholder='password'></input>
+                    <text id='pw-error' className='alert alert-danger'>
+                        <font color="red">{this.state.pw}</font></text><br/><br/>
                     <button type="button" className="btn" download onClick={this.onClickHandler}>Download</button>
-                    <p><button type="button" className="btn" onClick={this.test}>test</button></p>
                 </div>
-            </div>
-        )
+            </div>);
+
+        const failTemplate = (<div>
+            <h1>Download Page</h1>
+                <div> 
+                    File Not Found<br/>
+                    Id:   {this.props.match.params.fileId}<br/>
+                </div>
+            </div>);
+
+        return (this.state.document.fileName? successTemplate:failTemplate)
     }
 }
 
--- routes/db.js
+++ routes/db.js
@@ -17,9 +17,7 @@ const crypto = require('crypto');
 
 router.post("/upload",  function(req, res) {
     
-    console.log('*** arived in post db/upload ***');
     const form = new formidable.IncomingForm();
-    
     form.parse(req, async function(err, fields, files) {
   		// console.log(fields);
   		// console.log(files);
@@ -44,102 +42,32 @@ router.post("/upload",  function(req, res) {
   		fs.createReadStream( files['file'].path ).pipe( writestream );
   		
   		writestream.on('close',function(file){
-  			console.log("file added to db");
+  			// console.log("file added to db");
   		});
-		
+  		writestream.on("error", function(err) { 
+	    	console.error('error');
+    		res.status(500).end();
+		});
   		
-  		return res.status(200)
+  		return res.status(201)
     	.send(docId);
 	});
-
-    
-    // const data = req.body;
-    // console.log(data);
-    // return res.status(500)
-    // .send({});
-    
 });
 
 router.get("/:documentCode", async function(req,res){
-	console.log('*** arived in get db/:doc ***');
-	const docId = req.params.documentCode;
 
+	const docId = req.params.documentCode;
 	const document = await Document.findOne({docId:docId}, function (err, doc) {
-		if (err) return console.error(err);
+		if (err) {
+			// console.error(err);
+			res.status(404).send('Error getting Document from Database');
+		}
 	});
 
 	res.status(200).send({
 		fileName:document.fileName,
 		fileType:document.fileType,
 		expirationDate: document.expirationDate
-	});
-});
-
-
-router.get("/download/:documentCode", async function(req,res){
-	console.log('*** arived in get db/download ***');
-	const docId = req.params.documentCode;
-
-	const form = new formidable.IncomingForm();
-	form.parse(req, async function(err, fields, files) {
-
-    	console.log("fields:",fields);
-    });
-
-
-	const document = await Document.findOne({docId:docId}, function (err, doc) {
-		if (err) return console.error(err);
-	});
-
-	const gridfs = await Grid(connection.db, mongoose.mongo);
-
-	gridfs.findOne( {filename:docId}, function (err,file){
-		
-		if (err) return res.status(400).send(err);
-		else if (!file) {
-        	return res.status(404).send('Error on the database looking for the file.');
-    	}
-		// return res.status(200).download( file );
-
-		res.set('Content-Type', document.fileType);
-    	res.set('Content-Disposition', 'attachment; filename="' + document.fileName +  '"');
-
-    	var readstream = gridfs.createReadStream({filename:docId});
-
-    	readstream.on("error", function(err) { 
-        	res.end();
-    	});
-    	readstream.pipe(res);
-	});
-
-	// return res.status(200).send(document.filename);
-});
-
-
-router.get("/test", async function(req,res){
-	console.log('*** arived in get db/test ***');
-
-	console.log('hash: ' + crypto.createHash('sha256').update('password').digest('hex'));
-	console.log('hash: ' + crypto.createHash('sha256').update('password').digest('hex'));
-
-	console.log('*** ended get db/test ***');
-	return res.status(200);
-
-});
-
-router.get("/test/:argument", async function(req,res){
-
-	const form = new formidable.IncomingForm();
-    
-    form.parse(req, async function(err, fields) {
-    	console.log("pw: ",fields);
-		const hash1 = crypto.createHash('sha256').update('password').digest('hex');
-		const hash2 = crypto.createHash('sha256').update(fields.password).digest('hex');
-		console.log('hash: ' + hash1 );
-		console.log('hash: ' + hash2 );
-		console.log( hash1 == hash2 );
-
-		return res.status(200).send(hash1 == hash2);
 	});
 });
 
@@ -163,36 +91,32 @@ router.post("/file/:documentCode", async function(req,res){
 						.update(fields.password)
 						.digest('hex');
 
-		if( hash == document.hashedKey ){
-			const gridfs = await Grid(connection.db, mongoose.mongo);
+		if( hash != document.hashedKey ) return res.status(401).send('Bad password');
 
-			gridfs.findOne( {filename:docId}, function (err,file){
-				
-				if (err) return res.status(400).send(err);
-				else if (!file) {
-		        	return res.status(404).send('Error on the database looking for the file.');
-		    	}
-				// return res.status(200).download( file );
+		const gridfs = await Grid(connection.db, mongoose.mongo);
 
-				res.set('Content-Type', document.fileType);
-		    	res.set('Content-Disposition', 'attachment; filename="' + document.fileName +  '"');
+		gridfs.findOne( {filename:docId}, function (err,file){
+			
+			if (err) return res.status(400).send(err);
+			else if (!file) {
+	        	return res.status(404).send('Error on the database looking for the file.');
+	    	}
 
-		    	var readstream = gridfs.createReadStream({filename:docId});
+			res.set('Content-Type', document.fileType);
+	    	res.set('Content-Disposition', 'attachment; filename="' + document.fileName +  '"');
 
-		    	readstream.on("error", function(err) { 
-		        	res.end();
-		    	});
-		    	readstream.pipe(res);
+	    	var readstream = gridfs.createReadStream({filename:docId});
 
-		    	readstream.on("error", function(err) { 
-		    		console.error('error');
-        			res.end();
-				});
+	    	readstream.on("error", function(err) { 
+	        	res.end();
+	    	});
+	    	readstream.pipe(res);
+
+	    	readstream.on("error", function(err) { 
+	    		console.error('error');
+    			res.end();
 			});
-		}
-		else {
-			console.log('bad password');
-			return res.status(200).send('Bad Password');}
+		});
 	});
 });
 
