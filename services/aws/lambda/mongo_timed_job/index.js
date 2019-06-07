30dd4cc3841493abac9adb28af9ecaa9de01c082
Andrew Patterson <apat24@mail.sfsu.edu>
2019-06-07 12:57:06 -0700

    installs moment, fixes dotenv initialization

--- services/aws/lambda/mongo_timed_job/index.js
+++ services/aws/lambda/mongo_timed_job/index.js
@@ -1,15 +1,26 @@
-// require("dotenv").load();
+require("dotenv").config({ path: __dirname + "/.env" });
 const mongoose = require("mongoose");
 
-exports.handler = async event => {
-  // TODO implement
-  console.log("MONGO_URI:", process.env.MONGO_URI);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Lambda!",
-      uri: process.env.MONGO_URI
-    })
-  };
-  return response;
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+const test = async () => {
+  const thing = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  });
+  console.log(thing);
 };
+
+test();
+
+// exports.handler = async event => {
+//   // TODO implement
+//   console.log("MONGO_URI:", process.env.MONGO_URI);
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Hello from Lambda!",
+//       uri: process.env.MONGO_URI
+//     })
+//   };
+//   return response;
+// };
