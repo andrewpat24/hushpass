a61d2e2e0af2d46bfabf7bafb800cec7b979b1f3
Andrew Patterson <apat24@mail.sfsu.edu>
2019-06-10 21:52:36 -0700

    adds models folder for documents. Removes documents with expired dates.

--- services/aws/lambda/mongo_timed_job/index.js
+++ services/aws/lambda/mongo_timed_job/index.js
@@ -1,20 +1,70 @@
 require("dotenv").config({ path: __dirname + "/.env" });
+const moment = require("moment");
+// Mongoose connection
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+require("./models/Documents");
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
+const Grid = require("gridfs-stream");
+eval(
+  `Grid.prototype.findOne = ${Grid.prototype.findOne
+    .toString()
+    .replace("nextObject", "next")}`
+);
 
-exports.handler = async event => {
-  // TODO implement
-  console.log("MONGO_URI:", process.env.MONGO_URI);
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+const removeExpiredDocuments = async () => {
+  let expiredDocsData = {
+    numberOfDocs: 0,
+    docMetadata: new Array()
+  };
+
+  const mongooseConn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true
   });
-  const gridfs = await Grid(connection.db, mongoose.mongo);
+  const Document = mongooseConn.model("documents");
+  const currentDate = moment().format();
 
+  const documents = await Document.find(
+    {
+      expirationDate: {
+        $lte: currentDate
+      }
+    },
+    (err, doc) => {
+      if (err) {
+        // console.error(err);
+        res.status(404).send("Error retrieving Document from Database.");
+      }
+    }
+  );
+
+  console.log(documents.length);
+  documents.forEach(doc => {
+    expiredDocsData.numberOfDocs += 1;
+    expiredDocsData.docMetadata.push(doc);
+
+    doc.remove((err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res);
+      }
+    });
+  });
+
+  console.log(expiredDocsData);
+  return expiredDocsData;
+};
+
+removeExpiredDocuments();
+
+exports.handler = async event => {
+  const expiredDocsData = removeExpiredDocuments();
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Lambda!"
+      message: `${expiredDocsData.numberOfDocs} document(s) have been removed.`,
+      docData: expiredDocsData.docMetadata
     })
   };
   return response;
---
+++ services/aws/lambda/mongo_timed_job/models/Documents.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+const Schema = mongoose.Schema;
+
+const documentsSchema = new Schema({
+  docId: { type: String, trim: true },
+  data: {},
+  fileName: { type: String, trim: true, default: "file" },
+  fileType: { type: String },
+  hashedKey: String,
+  expirationDate: {
+    type: Date,
+    default: moment().add(1, "d")
+  },
+  maxDownloads: { type: Number, default: 1 },
+  downloadCount: { type: Number, default: 0 },
+  dateCreated: { type: Date, default: moment() },
+  userID: Number,
+  valid: { type: Boolean, default: true }
+});
+
+mongoose.model("documents", documentsSchema);
