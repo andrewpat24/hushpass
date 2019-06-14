47735694ba3d234926d097b84666b7430f9f9902
Andrew Patterson <apat24@mail.sfsu.edu>
2019-06-14 13:56:51 -0700

    updated wording in introduction component

--- client/src/components/Introduction.js
+++ client/src/components/Introduction.js
@@ -4,17 +4,19 @@ const Introduction = (
   <section component="Introduction">
     <div className="border">
       <p>
-        <span className="bold">HushHush</span> is a service to securly share
-        files. You upload a file, set a secret key, tell us how long you need it
-        available and how many times it can be downloaded. We then provide a
+        <span className="bold">HushHush</span> is a service for sharing files
+        securely. Upload your file, set a secret key, tell us how long you need
+        it available, and how many times it may be downloaded. We then provide a
         link to download the file using the secret key as a password.
       </p>
       <p>
         Before we store your file, we encrypt it using your secret key. We do
-        not save they key in a way we can use, so your file is safely encrypted
-        untill the secret key is provided to download it. When downloading, as
-        soon as we have the correct secret key, we send the file to you as we
-        decrypt it.
+        not save your key in a way we can use, so your file is safely encrypted
+        until the secret key is provided to download. As soon as we have the
+        secret key, we decrypt your file and send it to you. Once the file has
+        either reached its maximum number of downloads or reached its expiration
+        date, we delete the file's data from our database so it may never be
+        accessed again.
       </p>
       <p>
         We saw a need to send sensitive documents and feel safe about it. Other
